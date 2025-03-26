2d3c0dfbc38c76bfad8d05f954492cef8fcc8108
Thomas Epperson <thomas.epperson@gmail.com>
2025-03-26 17:13:07 -0500

    Remove the sinc code.

--- image_proc/src/main.rs
+++ image_proc/src/main.rs
@@ -1,10 +1,5 @@
-use std::f64::consts::PI;
-
 use eframe::CreationContext;
 use egui_plot::{Line, Plot, PlotPoints};
-use splines::interpolate::Interpolate;
-
-mod widget1;
 
 struct MainData {
     scale: Vec<f64>,
@@ -15,28 +10,6 @@ impl MainData {
         Self {
             scale: vec![0.0; 16],
         }
-    }
-}
-
-fn sinc(x: f64) -> f64 {
-    if x == 0.0 {
-        1.0
-    }
-    else {
-        (x * PI).sin() / (x * PI)
-    }
-}
-
-fn sinc_interpolate(indata: &[f64], odata: &mut [f64]) {
-    let max_out = odata.len() - 1;
-    for (i, elem) in odata.iter_mut().enumerate() {
-        let o_frac = i as f64 / max_out as f64;
-        let i_index = o_frac * (indata.len() - 1) as f64;
-        let mut total : f64 = 0.0;
-        for (index, in_element) in indata.iter().enumerate() {
-            total += *in_element * sinc(index as f64 - i_index);
-        }
-        *elem = total;
     }
 }
 
@@ -57,7 +30,6 @@ impl eframe::App for MainData {
                 *e = a;
             }
             let a: PlotPoints = points_out.iter_mut().enumerate().map(|(i, v)| {[time_scale * i as f64, *v]}).collect();
-            sinc_interpolate(&less_points, &mut points_out);
             let plot: PlotPoints = less_points.iter().enumerate().map(|a| {
                 [a.0 as f64, *a.1]
             }).collect();
